d640bbe2b1c056d1196254c4bc60013456b85b5a
Adriton1 <58088258+Adriton1@users.noreply.github.com>
2022-05-11 19:18:44 +0200

    Subida de proyecto

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2022 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+    const currentLocation = location.href;
+    const menuItem = document.querySelectorAll('a');
+    const menuLength = menuItem.length;
+    const icono = document.querySelector('#icono');
+
+    for(let i = 0; i<menuLength; i++){
+        if(menuItem[i].href === currentLocation){
+            menuItem[i].classList = "nav-link active"
+        icono.className = "navbar-brand ps-3"
+        }
+    }
+
+});
