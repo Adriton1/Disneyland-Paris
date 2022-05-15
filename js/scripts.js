753a5f51c601b3381678048686a8992474bafba2
Adriton1 <58088258+Adriton1@users.noreply.github.com>
2022-05-15 18:23:53 +0200

    actualizacion de postal, alerta y cambio de estilo

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,25 @@ window.addEventListener('DOMContentLoaded', event => {
         }
     }
 
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+    const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-success" alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+    }
+
+    const alertTrigger = document.getElementById('liveAlertBtn')
+    if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        alert('Se ha enviado la postal correctamente')
+    })
+    }
+
 });
