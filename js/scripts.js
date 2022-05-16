4cb878751f65c4c326fe895b3f392cdce71246c6
Adriton1 <58088258+Adriton1@users.noreply.github.com>
2022-05-16 18:15:42 +0200

    funcionalidad boton eliminar estilos

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,8 @@ window.addEventListener('DOMContentLoaded', event => {
 
     for(let i = 0; i<menuLength; i++){
         if(menuItem[i].href === currentLocation){
-            menuItem[i].classList = "nav-link active"
+            menuItem[i].classList.add("nav-link"); 
+            menuItem[i].classList.add("active");
         icono.className = "navbar-brand ps-3"
         }
     }
@@ -61,7 +62,26 @@ window.addEventListener('DOMContentLoaded', event => {
       });
 
 
-    const margen = document.getElementsByClassName("prueba")
+
+
+
+
+
+
+      const letra = document.getElementsByClassName("letra")
+      const navbarLeft = document.getElementsByClassName("recuperarLeftNav")
+      const ini = document.getElementById("ini")
+      const atracc = document.getElementById("atracc")
+      const post = document.getElementById("post")
+      const borde = document.getElementsByClassName("recuperarBorde")
+      const menuRapido = document.getElementsByClassName("recuperarMenu")
+  
+      var bordeLen = 5;
+      var margenLength = 1;
+      var navbarLeftLength = 1;
+      var menuRapidoLength = 1;
+
+    const margen = document.getElementsByClassName("recuperar")
     document.querySelector(".side-panel-toggle").addEventListener("click", () => {
     for(var i = 0; i<margen.length; i++){
         if(margen[i].classList.contains('margenAbierto')){
@@ -75,15 +95,36 @@ window.addEventListener('DOMContentLoaded', event => {
     }
     });
 
+    document.querySelector("#eliminar").addEventListener("click", () => {
+        document.styleSheets[0].disabled = true;
+        document.body.removeAttribute("class");
+        for (var i = 0; i< margenLength;i++){
+            margen[i].classList.remove("sb-topnav");
+            margen[i].classList.remove("navbar"); 
+            margen[i].classList.remove("navbar-expand"); 
+            margen[i].classList.remove("navbar-dark"); 
+            margen[i].classList.remove("bg-dark"); 
+            margen[i].classList.remove("margenAbierto"); 
+            margen[i].classList.remove("prueba");  
+        }
+        for (var i = 0; i< navbarLeftLength;i++){
+            navbarLeft[i].classList.remove("sb-sidenav");
+            navbarLeft[i].classList.remove("accordion");
+            navbarLeft[i].classList.remove("sb-sidenav-dark");
+            navbarLeft[i].classList.remove("sidenavAccordion");
+        }
+        ini.classList.remove('inicio');
+        atracc.classList.remove('atraccion');
+        post.classList.remove('postal');
+        for(var i =0; i< bordeLen; i++){
+            borde[i].classList.remove('borde');   
+        }
+        for(var i =0; i< menuRapidoLength; i++){
+            menuRapido[i].classList.remove("bg-light");
+        }
+    });
 
-
-    const letra = document.getElementsByClassName("letra")
-    const navbarLeft = document.getElementsByClassName("sidenavAccordion")
-    const ini = document.getElementById("inicio")
-    const atracc = document.getElementById("atracciones")
-    const post = document.getElementById("postal")
-    const borde = document.getElementsByClassName("borde")
-    const menuRapido = document.getElementsByClassName("menuRapido")
+    
     document.addEventListener("keydown", function(event){
         var key = event;
         if (key.keyCode == "80") {
@@ -104,21 +145,57 @@ window.addEventListener('DOMContentLoaded', event => {
         if (key.keyCode == "69"){
             document.styleSheets[0].disabled = true;
             document.body.removeAttribute("class");
-            for (var i = 0; i< margen.length;i++){
-                margen[i].removeAttribute("class");
+            for (var i = 0; i< margenLength;i++){
+                margen[i].classList.remove("sb-topnav");
+                margen[i].classList.remove("navbar"); 
+                margen[i].classList.remove("navbar-expand"); 
+                margen[i].classList.remove("navbar-dark"); 
+                margen[i].classList.remove("bg-dark"); 
+                margen[i].classList.remove("margenAbierto"); 
+                margen[i].classList.remove("prueba");  
             }
-            for (var i = 0; i< navbarLeft.length;i++){
-                navbarLeft[i].removeAttribute("class");
+            for (var i = 0; i< navbarLeftLength;i++){
+                navbarLeft[i].classList.remove("sb-sidenav");
+                navbarLeft[i].classList.remove("accordion");
+                navbarLeft[i].classList.remove("sb-sidenav-dark");
+                navbarLeft[i].classList.remove("sidenavAccordion");
             }
-            ini.removeAttribute('id');
-            atracc.removeAttribute('id');
-            post.removeAttribute('id');
-            var bordeLength = borde.length;
-            for(var i =0; i< bordeLength; i++){
-                borde[0].classList.remove('borde');
+            ini.classList.remove('inicio');
+            atracc.classList.remove('atraccion');
+            post.classList.remove('postal');
+            for(var i =0; i< bordeLen; i++){
+                borde[i].classList.remove('borde');   
             }
-            for(var i =0; i< menuRapido.length; i++){
+            for(var i =0; i< menuRapidoLength; i++){
                 menuRapido[i].classList.remove("bg-light");
+            }
+        }
+
+        if (key.keyCode == "82"){
+            document.body.classList.add("sb-nav-fixed");
+            for (var i = 0; i< margenLength;i++){
+                margen[i].classList.add("sb-topnav");
+                margen[i].classList.add("navbar"); 
+                margen[i].classList.add("navbar-expand"); 
+                margen[i].classList.add("navbar-dark"); 
+                margen[i].classList.add("bg-dark"); 
+                margen[i].classList.add("margenAbierto"); 
+                margen[i].classList.add("prueba"); 
+            }
+            for (var i = 0; i< navbarLeftLength;i++){
+                navbarLeft[i].classList.add("sb-sidenav");
+                navbarLeft[i].classList.add("accordion");
+                navbarLeft[i].classList.add("sb-sidenav-dark");
+                navbarLeft[i].classList.add("sidenavAccordion");
+            }
+            ini.classList.add('inicio');
+            atracc.classList.add('atraccion');
+            post.classList.add('postal');
+            for(var i =0; i< bordeLen; i++){
+                borde[i].classList.add('borde');
+            }
+            for(var i =0; i< menuRapidoLength; i++){
+                menuRapido[i].classList.add("bg-light");
             }
         }
     })
